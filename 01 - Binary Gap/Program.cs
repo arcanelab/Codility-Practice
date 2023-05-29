511d5502300348dc6eb281aaf9b9a8ef2f7f396b
Zoltán Majoros <zoltan@arcanelab.com>
2023-05-29 11:15:43 +0200

    Added Binary gap project

---
+++ 01 - Binary Gap/Program.cs
@@ -0,0 +1,43 @@
+﻿
+public class BinaryGapSolver
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int number = Math.Abs(new Random().Next());
+            int lbg = CalculateLongestBinaryGap(number);
+            Console.WriteLine($"{Convert.ToString(number, 2)} = {lbg}");
+        }
+    }
+
+    static int CalculateLongestBinaryGap(int number)
+    {
+        const int bits = sizeof(int) * 8;
+        int longestBinaryGap = 0;
+        int currentBinaryGap = 0;
+        bool counting = false;
+
+        for (int i = 0; i < bits; i++)
+        {
+            // lSB = least significant bit
+            bool LSB = ((number >> i) & 1) == 1;
+
+            if (LSB)
+            {
+                counting = true;
+                longestBinaryGap = Math.Max(longestBinaryGap, currentBinaryGap);
+                currentBinaryGap = 0;
+            }
+            else
+            {
+                if (counting)
+                {
+                    currentBinaryGap++;
+                }
+            }
+        }
+
+        return longestBinaryGap;
+    }
+}
